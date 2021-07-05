14140ebf8ca4d6b5483c2ec5f9390e528f15b586
aravindan a <aravindan@aravindans-MacBook-Pro.local>
2021-07-05 10:48:58 +0530

    script changes

--- script/customScript.sh
+++ script/customScript.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
 
-touch /tmp/testing.txt
+sudo apt-get -y update
+sudo apt install docker.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo groupadd docker
+sudo usermod -aG docker ${USER}
+docker pull selahcloud/newbadstore:latest
+docker run -it -p 80:80 selahcloud/newbadstore:latest

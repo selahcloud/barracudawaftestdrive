a7f1583dbc4fd5cd8aff2089293de56239c46f45
aravindan a <aravindan@aravindans-MacBook-Pro.local>
2021-07-05 11:35:29 +0530

    script changes

--- script/customScript.sh
+++ script/customScript.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 
+cd /home/${USER}
 sudo apt-get -y update
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get -y update
+sudo apt-get -y install docker-ce
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo groupadd docker
